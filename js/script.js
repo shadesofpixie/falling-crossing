320ff038709bcc86a2d3baf533a355dd72aea29c
smilesandtea <smilesandcode@gmail.com>
2020-04-18 17:41:36 -0400

    Completed adding images.

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+
+var c = document.getElementById('canvas')
+var s = sprinkler.create(c) 
+function rainingTea(message) {
+    
+    var images = [
+        'tom-nook.png',
+        'peach.png',
+        'isabelle.png'
+      ]
+      var stop = s.start(images, {
+        imagesInSecond: 8,
+        ddyMin: 200,
+        ddyMax: 200,
+
+      })
+
+      setTimeout(function(){
+        stop();
+      }, 5000)
+
+}
+
+
+
+
+ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+    if( command === "teaparty" &&
+        ( !rewardId || flags.broadcaster || flags.mod ) ) {
+        rainingTea( message );
+    }
+    if( flags.customReward && extra.customRewardId === rewardId ) {
+        rainingTea( message );
+    }
+};
+ComfyJS.onChat = ( user, message, flags, self, extra ) => {
+    if( flags.customReward && extra.customRewardId === "f1d2db18-77eb-4f0c-a0d7-5790cc735f01" ) {
+        rainingTea( message );
+    }
+};
+ComfyJS.Init( "shadesofpixie" );
